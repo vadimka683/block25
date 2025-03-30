5839f15aba6df11260158a49d2d7db00cd744886
vivasuk683 <vivas@DESKTOP-BE431TS>
2025-03-30 16:01:34 +0300

    Добавьте файлы проекта.

---
+++ Задача1/include/Items.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <iostream>
+
+void scalpel(const double& X, const double& Y) {
+	std::cout << "Cut between" << X << " and " << Y << "\n";
+}
+void hemostat(const double& X) {
+	std::cout << "Hemostat in " << X << "\n";
+}
+void tweezers(const double& X) {
+	std::cout << "Tweezers in " << "X" << "\n";
+}
+bool suture(const double& X1, const double& Y1, const double& X2, const double& Y2) {
+	if (X1 != X2 || Y1 != Y2) {
+		return false;
+	}
+	std::cout << "Suture between" << X2 << " and " << Y2 << "\n";
+	return true;
+}
---
+++ Задача1/src/main1.cpp
@@ -0,0 +1,34 @@
+﻿#include "Items.h"
+#include<iostream>
+
+int main()
+{
+	double X1, Y1;
+	std::cout << "Input X1 and X2 for scalpel: ";
+	std::cin >> X1 >> Y1;
+	scalpel(X1, Y1);
+	int comand;
+	do {
+		std::cout << "Input number comand: 1.hemostat 2.tweezers 3.suture: ";
+		std::cin >> comand;
+		if (comand == 1) {
+			std::cout << "Input the hemostat coordinate: ";
+			double X;
+			std::cin >> X;
+			hemostat(X);
+		}
+		else if (comand == 2) {
+			std::cout << "Input the tweezers coordinate: ";
+			double X;
+			std::cin >> X;
+			hemostat(X);
+		}else if (comand == 3) {
+			std::cout << "Input X1 and X2 for suture: ";
+			double X2, Y2;
+			std::cin >> X2 >> Y2;
+			if (suture(X1, Y1, X2, Y2)) {
+				break;
+			}
+		}
+	} while (true);
+}
